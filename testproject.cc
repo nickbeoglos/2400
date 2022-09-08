0fb22d0aa65452286c27fe109443bd727a3cbf60
Nick <nb975422@ohio.edu>
2022-09-08 12:45:48 -0400

    Coding project commit

---
+++ testproject.cc
@@ -0,0 +1,17 @@
+// Trying to test code real quick!!
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    int numofdogs = 0;
+
+    cout << "How many dogs do you have?" << endl;
+    cin >> numofdogs;
+    cout << "You have " << numofdogs << " dogs!";
+    
+    return 0;
+}
